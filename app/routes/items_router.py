a7aeb52ade65b4e3ad3befe1093f265198a5833c
qwewwq <ironou7@gmail.com>
2026-05-06 17:05:24 +0300

    make some endpoint secure

--- app/routes/items_router.py
+++ app/routes/items_router.py
@@ -2,8 +2,11 @@ from decimal import Decimal
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.utils import get_current_user
 from app.core.database import get_db
-from app.schemas.item_schema import ItemOut, ItemCreate
+from app.models.user_models import User
+from app.schemas.item_schema import ItemOut
 from app.schemas.coin_schema import CoinCreate
 from app.cruds import item_crud, coin_crud
 
@@ -17,9 +20,10 @@ router = APIRouter(
 async def create_item(
     coin: CoinCreate,
     target_price: int,
-    user_id: int,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user)
 ):
+    user_id = user.id
     return await coin_crud.create_coin(coin=coin, db=db, target_price=target_price, user_id=user_id)
 
 
@@ -29,13 +33,22 @@ async def get_multiple_items(db: AsyncSession = Depends(get_db)):
     return items
 
 @router.get('/read_all', response_model=list[ItemOut])
-async def read_all_items(db: AsyncSession = Depends(get_db)):
-    return await item_crud.read_items(db=db)
+async def read_all_items(
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    user_id = user.id
+    return await item_crud.read_items(db=db, user_id=user_id)
 
 
 @router.delete('/delete_item/{item_id}')
-async def delete_item(item_id: int, db: AsyncSession = Depends(get_db)):
-    await item_crud.delete_item(item_id=item_id, db=db)
+async def delete_item(
+        item_id: int,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    user_id = user.id
+    await item_crud.delete_item(item_id=item_id, db=db, user_id=user_id)
 
 @router.get('/all_history')
 async def get_all_history(db: AsyncSession = Depends(get_db)):
@@ -43,6 +56,17 @@ async def get_all_history(db: AsyncSession = Depends(get_db)):
 
 
 @router.put('/update_target_price')
-async def update_target_price(item_id: int, target_price: Decimal, db: AsyncSession = Depends(get_db)):
-    updated_item = await item_crud.update_target_price(item_id=item_id, target_price=target_price, db=db)
+async def update_target_price(
+        item_id: int,
+        target_price: Decimal,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    user_id = user.id
+    updated_item = await item_crud.update_target_price(
+        item_id=item_id,
+        target_price=target_price,
+        db=db,
+        user_id=user_id
+    )
     return updated_item
