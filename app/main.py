f24895ef0f936bba0d94375b9a33c4d1a6c14694
qwewwq <ironou7@gmail.com>
2026-05-06 17:04:16 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.routes import items_router, user_router, coin_router
+from app.routes import items_router, user_router, coin_router, login_router
 
 app = FastAPI()
 
 app.include_router(items_router.router)
 app.include_router(user_router.router)
 app.include_router(coin_router.router)
+app.include_router(login_router.router)
 
 
 @app.get('/')
